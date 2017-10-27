0aa15bc6b4936ce9679b9b1c6d43ba891a85f674
jacksonpradolima <jacksonpradolima@gmail.com>
2017-10-27 17:41:45 -0200

    update gsp

--- gsp.py
+++ gsp.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 """
 ===============================================
 GSP (Generalized Sequential Pattern) algorithm
@@ -18,7 +17,6 @@ transactions = [
 				['Bread', 'Milk', 'Diaper', 'Coke']
 			]
 """
-# print(__doc__)
 
 import logging
 import numpy as np
@@ -32,53 +30,47 @@ __version__ = "1.0"
 
 class GSP:
 	def __init__(self, raw_transactions):
+		self.freq_patterns = []
 		self._pre_processing(raw_transactions)
 
 	def _pre_processing(self, raw_transactions):
 		'''
 		Prepare the data
+
+		Parameters:
+			raw_transactions: the data that it will be analysed
 		'''
 		# each item is parsed to a set type
 		self.transactions = [set(np.array(i)) for i in raw_transactions]
 
 		# different items in the base
-		self.unique_candidates = set(x for l in self.transactions for x in l)
+		self.unique_candidates = [set([item]) for item in set(x for l in self.transactions for x in l)]
 
-	def _frequency(self, items):
-		'''
-		Frequency of occurrence of a set of items in the base
-
-		Parameters
-			items: set of items that will be evaluated
-		'''
-
-		results = {}
-		for item in items:
-			freq = 0
-			aux = set([item]) if type(item) != set else item
-			for t in self.transactions:
-				if aux.issubset(t):
-					freq += 1
-			results[tuple(aux)] = freq
-		return results
+		# the total of different items in the base
+		self.t_size = len(self.unique_candidates)
 
 	def _support(self, items, minsup = 0):
 		'''
 		The support is the probability that the antecedent of the rule is present in the base (transactions) in relation to the total amount of records in the base.
+
+		Parameters
+			items: set of items that will be evaluated
+			minsup: minimum support
 		'''
-
 		results = {}
-		t_size = len(self.unique_candidates)
+		for item in items:
+			# The number of times the item appears in the base
+			frequency = len([t for t in self.transactions if item.issubset(t)])
 
-		for key, freq in items.items():
-			# ratio between the number of times the item appears in the base by the total of different items in the base
-			qnt = freq / t_size
-			if qnt > minsup:
-				results[key] = qnt
+			# ratio between the frequency and the total of different items in the base
+			support = round(frequency / self.t_size, 3)
+
+			if support >= minsup:
+				results[tuple(item)] = [frequency, support]
 		return results
 
-	def _print_status(self, run, candidates_size, new_freq_patterns_size):
-		logging.debug("Run {}\nThere are {} candidates.\nThe candidates have been filtered down to {}.\n".format(run, candidates_size, new_freq_patterns_size))
+	def _print_status(self, run, candidates):
+		logging.debug("Run {}\nThere are {} candidates.\nThe candidates have been filtered down to {}.\n".format(run, len(candidates), len(self.freq_patterns[run-1])))
 
 	def search(self, minsup = 0.2):
 		'''
@@ -89,30 +81,26 @@ class GSP:
 		'''
 		assert (0.0 < minsup) and (minsup <= 1.0)
 
-		# initial candidates: all singleton sequences (k-itemsets/k-sequence = 1) - Initially, every item in DB is a candidate
+		# the set of frequent 1-sequence: all singleton sequences (k-itemsets/k-sequence = 1) - Initially, every item in DB is a candidate
 		candidates = self.unique_candidates
 
 		# scan transactions to collect support count for each candidate sequence & filter
-		freq_patterns = new_freq_patterns = self._support(self._frequency(candidates), minsup)
+		self.freq_patterns.append(self._support(candidates, minsup))
 
 		# (k-itemsets/k-sequence = 1)
 		k_items = 1
 
-		self._print_status(k_items, len(candidates), len(new_freq_patterns))
+		self._print_status(k_items, candidates)
 
 		# repeat until no frequent sequence or no candidate can be found
-		while True:
-			# is there anything left?
-			if new_freq_patterns:
-				freq_patterns = new_freq_patterns
-				k_items += 1
-			else:
-				return freq_patterns
+		while len(self.freq_patterns[k_items - 1]):
+			k_items += 1
 
-			# if any left, generate new candidates from the "best" supports
-			candidates = [set(c) for c in combinations(set(x for l in new_freq_patterns.keys() for x in l), k_items)]
+			# Generate candidate sets Ck (set of candidate k-sequences) - generate new candidates from the last "best" candidates filtered by minimum support
+			candidates = [set(c) for c in combinations(set(x for l in self.freq_patterns[k_items - 2].keys() for x in l), k_items)]
 
 			# candidate pruning - eliminates candidates who are not potentially frequent (using support as threshold)
-			new_freq_patterns = self._support(self._frequency(candidates), minsup)
+			self.freq_patterns.append(self._support(candidates, minsup))
 
-			self._print_status(k_items, len(candidates), len(new_freq_patterns))
+			self._print_status(k_items, candidates)
+		return self.freq_patterns
