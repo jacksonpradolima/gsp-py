4adbd1bfb82591d5c2cfad9e3e676c2732bd2cbe
Jackson Antonio do Prado Lima <jacksonpradolima@gmail.com>
2024-12-28 00:28:07 -0300

    Update type annotations for unique candidates in GSP class
    Replaced specific type hints with more generic annotations (`Any`) for better flexibility when handling unique candidates. This ensures compatibility with a broader range of data types within the GSP implementation.

--- gsppy/gsp.py
+++ gsppy/gsp.py
@@ -86,7 +86,7 @@ Version:
 """
 import logging
 import multiprocessing as mp
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Any
 from itertools import chain
 from collections import Counter
 
@@ -171,7 +171,7 @@ class GSP:
         self.max_size = max(len(item) for item in raw_transactions)
         self.transactions: List[Tuple[str, ...]] = [tuple(transaction) for transaction in raw_transactions]
         counts: Counter[str] = Counter(chain.from_iterable(raw_transactions))
-        self.unique_candidates: list[tuple[str, ...]] = [(item,) for item in counts.keys()]
+        self.unique_candidates: list[tuple[str, Any]] = [(item,) for item in counts.keys()]
         logger.debug("Unique candidates: %s", self.unique_candidates)
 
     @staticmethod
